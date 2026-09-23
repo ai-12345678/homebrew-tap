28b417390213cc6ab0fcd102bbb6fa717ea8a98d
ai-12345678 <273522303+ai-12345678@users.noreply.github.com>
2026-09-23 18:31:46 +0800

    feat(go@1.27.1): add macOS amd64 support

--- Formula/go@1.27.1.rb
+++ Formula/go@1.27.1.rb
@@ -4,8 +4,15 @@ class GoAT1271 < Formula
   version "1.27.1"
 
   on_macos do
-    url "https://go.dev/dl/go1.27.1.darwin-arm64.tar.gz"
-    sha256 "ee215d57e0ec269c60cc9ceca68e6bda321ba9ee5afe24f4b0988703c2d87d12"
+    on_arm do
+      url "https://go.dev/dl/go1.27.1.darwin-arm64.tar.gz"
+      sha256 "ee215d57e0ec269c60cc9ceca68e6bda321ba9ee5afe24f4b0988703c2d87d12"
+    end
+
+    on_intel do
+      url "https://go.dev/dl/go1.27.1.darwin-amd64.tar.gz"
+      sha256 "8f8f52c6649542cf027bbc9b9c68d1ec042f9f34808a40413f0b8b3f66f3caa4"
+    end
   end
 
   on_linux do
